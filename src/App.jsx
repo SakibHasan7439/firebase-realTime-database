3e569a3f33e0b8c059d4a13412752c288dce71d0
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-07-18 11:31:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex justify-center items-center">
+      <h3 className="text-5xl font-light text-slate-800">firebase realtime database</h3>
+    </div>
+  )
+}
+
+export default App
